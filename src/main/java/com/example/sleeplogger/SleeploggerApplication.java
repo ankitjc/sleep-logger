d6a9845b80cec2577a74b8d91af066c1979a0cc6
ankitjc <ankitjc@gmail.com>
2026-06-26 14:15:46 -0400

    [Feat-101] initial setup for sleep logger

---
+++ src/main/java/com/example/sleeplogger/SleeploggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sleeplogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleeploggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleeploggerApplication.class, args);
+	}
+
+}
